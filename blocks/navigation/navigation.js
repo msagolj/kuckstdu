c7e98d98f6a3e0f14409de9b15691844bb240c7d
Mirko Sagolj <msagolj@adobe.com>
2022-10-06 12:54:09 +0200

    c

--- blocks/navigation/navigation.js
+++ blocks/navigation/navigation.js
@@ -1,7 +1,28 @@
 import { readBlockConfig, decorateIcons } from '../../scripts/scripts.js';
 export default function decorate(block) {
   const links = block.querySelectorAll('a');
-  links[0].prepend(" :nextarrow: ");
+
+  block.textContent = '';
+  const dom = document.createRange().createContextualFragment(`
+    <div class='navlinks'>
+      <div class='previous'>
+      :nextarrow:
+      </div>
+      <div class='next'>
+      :previousarrow:
+      </div>
+    </div>
+  `)
+
+  if (links[0]) {
+    dom.querySelector('.previous').append(links[0]);
+  }
+    
+  if (links[1]) {
+    dom.querySelector('.next').append(links[1]);
+  }
+
+  block.append(dom);
   decorateIcons(block);
   console.log(block)
 }
